60e2b800e68725333b0da6838692fd73a5f3a1f5
Pavel Linkesch <pavel.linkesch@gmail.com>
2014-12-18 17:44:45 +0100

    add support for external use

--- Assets/js/admin/pages.js
+++ Assets/js/admin/pages.js
@@ -2,6 +2,8 @@ var marvin_pages = function () {
     var editors = {};
 
     return {
+        $table = $('#pages'),
+
         init: function () {
             marvin_pages.events();
             marvin_pages.editor();
@@ -15,7 +17,6 @@ var marvin_pages = function () {
         move: function (e) {
             var $a = $(e.target),
                 $tr = $a.parents('tr'),
-                $table = $('#pages'),
                 $sibling;
 
             e.preventDefault();
@@ -28,10 +29,10 @@ var marvin_pages = function () {
                 $sibling.after($tr);
             }
 
-            if ($table.hasClass('striped-even')) {
-                $table.removeClass('striped-even');
+            if (this.$table.hasClass('striped-even')) {
+                this.$table.removeClass('striped-even');
             } else {
-                $table.addClass('striped-even');
+                this.$table.addClass('striped-even');
             }
 
             marvin_pages.hideMoveButtons();
@@ -40,10 +41,10 @@ var marvin_pages = function () {
         },
 
         hideMoveButtons: function () {
-            $('#pages .move-up, #pages .move-down').removeClass('hidden');
+            this.$table.find('.move-up, .move-down').removeClass('hidden');
 
-            $('#pages tbody tr:first .move-up').addClass('hidden');
-            $('#pages tbody tr:last .move-down').addClass('hidden');
+            this.$table.find('tbody tr:first .move-up').addClass('hidden');
+            this.$table.find('tbody tr:last .move-down').addClass('hidden');
         },
 
         editor: function () {
